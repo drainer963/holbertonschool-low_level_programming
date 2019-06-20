897b4888ee04e1d7c7a97bf637988104dbbbab8a
drainer963 <744@holbertonschool.com>
2019-06-20 13:28:13 -0700

    printing alpha with out q and o

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n, m;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	for (m = 'A'; m <= 'Z'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
