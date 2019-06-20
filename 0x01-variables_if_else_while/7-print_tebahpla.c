2d72e6d53fe5f119d10e3b87892b1a78769d8dc4
drainer963 <744@holbertonschool.com>
2019-06-20 15:52:49 -0700

    backwards alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
