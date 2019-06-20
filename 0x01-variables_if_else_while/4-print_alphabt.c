eec1b858bb49827ce18991bf76d2cd4bc3cb057d
drainer963 <744@holbertonschool.com>
2019-06-20 13:32:25 -0700

    printing alpha both cases

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; n++)
+{
+if (n == 'q' || n == 'e')
+{
+n++;
+}
+{
+putchar(n);
+}
+}
+putchar('\n');
+return (0);
+}
