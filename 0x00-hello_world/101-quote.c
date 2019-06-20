7a58c4d65c198925e7980cbd2597c1c8a12ffe9b
drainer963 <744@holbertonschool.com>
2019-06-19 18:19:48 -0700

    final draft

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: 1
  */
 
 int main(void)
 
 {
-perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 
 return (1);
 }
