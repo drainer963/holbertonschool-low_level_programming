86a45d60d1e493d6bad1e88e32ff2b683d26d2d0
drainer963 <744@holbertonschool.com>
2019-06-20 17:30:46 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,11 @@ int main(void)
 	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-		putchar(',');
-		putchar(' ');
+		if (n <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
