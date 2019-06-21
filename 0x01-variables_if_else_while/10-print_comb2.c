c8b2fd0fa2e7ea55bf3d0fe5891b6c8eda826c5e
drainer963 <744@holbertonschool.com>
2019-06-21 01:20:13 -0700

    print comb 1

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,15 +11,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100 ; i++)
+	for (i = 0; i <= 99; i++)
 	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		if (i < 99)
-		{
+		if (i != 99)
 			putchar(',');
-			putchar(' ');
-		}
 	}
 	putchar('\n');
 	return (0);
