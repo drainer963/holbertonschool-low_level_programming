9b942ca57242e929b5051546e45d31b909e53c22
drainer963 <744@holbertonschool.com>
2019-06-20 19:12:36 -0700

    print combo 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, l;
+
+	for (l = '0'; l <= '9'; l++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (j = i + 1; j <= '9'; j++)
+			{
+				putchar(l);
+				putchar(i);
+				putchar(j);
+				if (i != '8' || j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
+}
