848a95619ef35344928575ce0de24c0cc11a0bac
drainer963 <744@holbertonschool.com>
2019-06-25 06:44:26 -0700

    header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int x, int y);
+void times_table(void);
+
+#endif
