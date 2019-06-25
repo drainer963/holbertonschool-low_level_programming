a8cccd358371855fd23c009b735baa82d9015f2e
drainer963 <744@holbertonschool.com>
2019-06-25 06:45:49 -0700

    print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "header.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
