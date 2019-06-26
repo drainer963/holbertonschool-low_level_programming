3dce3edffbf2f16933e03b1f7430763ec1116e1c
drainer963 <744@holbertonschool.com>
2019-06-25 18:31:03 -0700

    update description

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,12 +13,12 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 59; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(':');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			putchar('\n');
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
