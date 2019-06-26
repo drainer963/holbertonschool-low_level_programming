5542c20d0d8d9ea91adf91b899d4303d80a8035d
drainer963 <744@holbertonschool.com>
2019-06-25 23:52:17 -0700

    OG battery st

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_to_98 - printing to 98
+ * @n: int
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	while (n > 98)
+	{
+		printf("%d,  ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("98\n");
+}
