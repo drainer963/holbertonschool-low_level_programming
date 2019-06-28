e5b61183c0e5d6532e07b9f6047dadc115a9bb1a
drainer963 <744@holbertonschool.com>
2019-06-28 09:19:58 -0700

    more nums

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,11 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i <= 15; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			l = (i / 10);
-			k = (i % 10);
-			if (l != 0)
-				_putchar(l + '0');
-			_putchar(k + '0');
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
 		}
 		_putchar('\n');
 	}
