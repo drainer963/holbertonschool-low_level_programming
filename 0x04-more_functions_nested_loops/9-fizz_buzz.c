ca47bcd05ee690acd95bbcac770788da1e5b6c2f
drainer963 <744@holbertonschool.com>
2019-06-28 11:15:45 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * fizzbuzz - check the code for Holberton School students.
+ * @i: int
+ *
+ * Return: Always 0.
+ */
+void fizzbuzz(int i)
+{
+	for (; i < 101; puts(i++ % 5 ? "" : "Buzz"))
+		printf(i % 3 ? i % 5 ? "%d" : "" : "Fizz", i);
+}
