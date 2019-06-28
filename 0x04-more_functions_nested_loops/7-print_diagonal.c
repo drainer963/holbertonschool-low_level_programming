87756d9e9a6cf59cb2f0c613a45a33f8404f4f9a
drainer963 <744@holbertonschool.com>
2019-06-27 20:53:55 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,24 +8,23 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-	for (j = 0; j <= n; j++)
-	{
-		for (i = 0; i <= n; i++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			if (i == j)
-				_putchar('\\');
-			else if (i < j)
-				_putchar(' ');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
