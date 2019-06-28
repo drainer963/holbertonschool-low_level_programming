ba1b4b10bb3554a52bfd22de0308a64c749306b5
drainer963 <744@holbertonschool.com>
2019-06-27 18:01:07 -0700

    print nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print nums
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+}
