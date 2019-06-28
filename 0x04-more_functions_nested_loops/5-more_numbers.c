1ef39bf065794a6596b9abde6b91059410e681bd
drainer963 <744@holbertonschool.com>
2019-06-27 18:08:15 -0700

    print more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - keep printing nums.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j, l, k;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			l = (i / 10);
+			k = (i % 10);
+			if (l != 0)
+				_putchar(l + '0');
+			_putchar(k + '0');
+		}
+		_putchar('\n');
+	}
+}
