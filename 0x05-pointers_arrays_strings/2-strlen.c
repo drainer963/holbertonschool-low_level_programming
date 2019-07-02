398d51383aacd4fed1ae03f32cdc17a43f84a21b
drainer963 <744@holbertonschool.com>
2019-07-02 13:42:23 -0700

    str len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the length of string
+ * @s: char
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i, l = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		l++;
+	}
+	return (l);
+}
