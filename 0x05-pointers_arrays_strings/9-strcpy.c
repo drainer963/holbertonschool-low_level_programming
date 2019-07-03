55512990ccdde282218ce74c9cd5190b39c2e74b
drainer963 <744@holbertonschool.com>
2019-07-02 17:09:06 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: char
+ * @src: char
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (*src != '\0')
+	{
+		dest[i] = *src;
+		i++;
+		src++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
