5a0bf5c76a3d4232c6e3f465bd8d5a7bb30a98ee
drainer963 <744@holbertonschool.com>
2019-07-04 11:32:32 -0700

    headers

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -13,6 +13,8 @@ void print_diagonal(int n);
 void print_line(int n);
 void print_square(int size);
 void fizzbuzz(int i);
-
+void print_triangle(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
