91832d3666a2a2859584941ed490d7b9d9d5c1cd
drainer963 <744@holbertonschool.com>
2019-07-04 11:41:55 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ void fizzbuzz(int i)
 	while (i < 101)
 	{
 		if (i % 15 == 0)
-			printf(" FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf(" Fizz ");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf(" Buzz ");
+			printf(" Buzz");
 		else
-			printf("%d ", i);
+			printf(" %d ", i);
 		i++;
 	}
 	putchar('\n');
