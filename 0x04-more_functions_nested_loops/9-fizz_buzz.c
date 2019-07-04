a1f3095cfe5827970860105fd374df60e7cb2825
drainer963 <744@holbertonschool.com>
2019-07-04 09:37:22 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,6 +8,9 @@
  */
 void fizzbuzz(int i)
 {
-	for (; i < 101; puts(i++ % 5 ? "" : "Buzz"))
-		printf(i % 3 ? i % 5 ? "%d" : "" : "Fizz", i);
+	for (; i < 101;)
+	{
+		puts(i++ % 5 ? "" : "Buzz");
+	}
+	printf(i % 3 ? i % 5 ? "%d" : "" : "Fizz", i);
 }
