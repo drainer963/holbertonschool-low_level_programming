0ecc23d16a1dcd4412b444117c680d1c80410a4e
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-07 03:57:14 +0000

    print ints using only putchar

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_number - print number using only putchar
+ * @n: int
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
