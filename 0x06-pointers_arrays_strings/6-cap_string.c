32dae0427e75248f6c8aad36c142bdd65aa8c065
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-07 02:37:10 +0000

    escape comma

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,12 +15,14 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 		for (l = 0; l < 14; l++)
 		{
 			if (s[i] == sep[l])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] = s[i + 1] - 32;
+					s[i + 1] -= 32;
 			}
 		}
 	}
