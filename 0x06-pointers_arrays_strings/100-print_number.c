a008d9b275cf1510092c87c8840a054566d4333d
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-07 04:09:20 +0000

    print ints using only putchar

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,12 +8,18 @@
  */
 void print_number(int n)
 {
+	unsigned int i;
+
 	if (n < 0)
 	{
-		n = -n;
+		i = -n;
 		_putchar('-');
 	}
+	else
+	{
+		i = n;
+	}
 	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
 }
