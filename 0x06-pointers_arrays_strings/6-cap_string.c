6e182747592f8c170a1a949e4453cbe856cf0cdb
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-07 01:06:06 +0000

    capitalize all words in string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalize all words in string
+ *
+ * @s: char pointer
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i, l;
+	char sep[] = {' ', '.', ';', ',', '!', '?', '"', '(', ')', '{', '}'
+		      , '\n', '\t'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (l = 0; l < 13; l++)
+		{
+			if (s[i] == sep[l])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
