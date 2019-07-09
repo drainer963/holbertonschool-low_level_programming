e079f5362050b8d94982db66e96d5d9470d9c830
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-09 22:29:51 +0000

    headers

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,5 +9,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *__strchar(char * s, char c);
 char _putchar(int n);
+char *_strstr(char *haystack, char *needle);
 
 #endif
