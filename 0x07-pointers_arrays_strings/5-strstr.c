cb4c09f664df335a97eb6c174e306185718b2498
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-09 22:43:54 +0000

    find needle in haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (*needle == haystack[i])
+		if (*needle == haystack[i] && *needle != '\0')
 			return (needle);
 	}
 	return (NULL);
