cce3dac3fc14c7412bf8bb41a19820ecd7405a1b
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-09 23:27:05 +0000

    memcopy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,14 +3,16 @@
 
 /**
  * _memcpy - copies to mem
- * @dest: the address of memory to print
- * @src: the size of the memory to print
+ * @dest: the dest
+ * @src: the source
  * @n: int
  * Return: Nothing.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	while (n--)
+	{
 		*dest++ = *src++;
+	}
 	return (dest);
 }
