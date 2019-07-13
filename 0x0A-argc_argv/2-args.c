4fcf9bb130588be89c10ea03626ba9d9084c2812
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-13 19:40:32 +0000

    print all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - it all starts here
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
