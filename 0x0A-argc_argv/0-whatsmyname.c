a8b6f2331bff2f3f8464656d74397991aea1c867
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-13 19:19:05 +0000

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - it all starts here
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
