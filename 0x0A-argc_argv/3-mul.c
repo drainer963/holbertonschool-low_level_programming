2c37c1903d62c03a7eb384634c9469bc6e9dc0ce
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-13 19:43:29 +0000

    multiply arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - it all starts here
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, sum;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	sum = n1 * n2;
+	printf("%d\n", sum);
+	return (0);
+}
