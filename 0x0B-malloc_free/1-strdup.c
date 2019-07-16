5929e3343f6a3744e43eb58be5022d98e55fedb0
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-16 21:11:47 +0000

    string duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in mem
+ * @str: char pointer
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *newstr;
+	char *tmp;
+	int len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len])
+	{
+		len++;
+	}
+	newstr = malloc(len + 1);
+	tmp = newstr;
+	while (*str)
+		*tmp++ = *str++;
+	*tmp = '\0';
+	return (newstr);
+}
