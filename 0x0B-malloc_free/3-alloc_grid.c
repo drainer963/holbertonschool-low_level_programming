d8a4a5bbb315c53b7ef8455ad7b9b5ec84fe5483
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-17 04:51:11 +0000

    allocate a 2d array and return pointer to it

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; ++i)
 	{
 		matrix[i] = malloc(sizeof(int *) * width);
-		if (!(*(matrix)))
+		if (!matrix[i])
 		{
 			while (i >= 0)
 			{
