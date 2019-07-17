4c0e359b23c5b3e509cd85dcafa372ffa4e95a2b
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-17 05:27:48 +0000

    allocate grid and return 2d array in pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,25 +11,29 @@
 int **alloc_grid(int width, int height)
 {
 	int **matrix;
-	int i = 0;
+	int i, j, l, k;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	matrix = malloc(sizeof(int *) * height);
 	if (!matrix)
 		return (NULL);
-	for (i = 0; i < height; ++i)
+	for (i = 0; i < height; i++)
 	{
-		matrix[i] = malloc(sizeof(int *) * width);
-		if (!matrix[i])
+		matrix[i] = malloc(width * sizeof(int));
+
+		if (matrix[i] == NULL)
 		{
-			while (i >= 0)
-			{
-				free(matrix[i - 1]);
-			}
+			for (j = 0; j < i; j++)
+				free(matrix[j]);
 			free(matrix);
 			return (NULL);
 		}
+	}
+	for (l = 0; l < height; l++)
+	{
+		for (k = 0; k < width; k++)
+			matrix[l][k] = 0;
 	}
 	return (matrix);
 }
