a7dd466187943185e9ab3e8f6f655e1250ad8af1
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-18 22:43:49 +0000

    adding new parm

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,14 +7,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array = malloc(nmemb * size);
+        char *array;
 	unsigned int i = 0;
 
-	if (!array)
-		return (NULL);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	for (i = 0; i <= size * nmemb; i++)
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size * nmemb; i++)
 		array[i] = 0;
 	return (array);
 }
