7bab1343ab796147d3987ded5abdf5eb886e73e9
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-18 21:28:54 +0000

    header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _strlen(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 
