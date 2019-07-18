194d553ce4a31101bd38b3788ffe3ebcb942ce42
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-18 22:15:21 +0000

    calloc implentation

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,14 +8,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *array = malloc(nmemb * size);
-	int i = 0;
+	unsigned int i = 0;
 
 	if (!array)
 		return (NULL);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	while (array[i])
-		array[i] = 0;
-	i++;
+	while (i < nmemb)
+		++i;
+	array[i] = 0;
 	return (array);
 }
