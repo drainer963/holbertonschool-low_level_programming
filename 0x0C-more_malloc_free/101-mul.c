721bcab6b2f4fbd526363cc34a68137871457a07
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-19 08:14:18 +0000

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,18 +6,18 @@
  *
  * Return: Always 0.
  */
- int main(int argc, char *argv[])
- {
-	 int n1, n2, sum;
+int main(int argc, char *argv[])
+{
+	size_t n1, n2, sum;
 
-	 if (argc != 3)
-	 {
-		 printf("%s\n", "Error");
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
 		 exit(98);
-	 }
-	 n1 = atoi(argv[1]);
-	 n2 = atoi(argv[2]);
-	 sum = n1 * n2;
-	 printf("%d\n", sum);
-	 return (0);
- }
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	sum = n1 * n2;
+	printf("%lu\n", (unsigned long) sum);
+	return (0);
+}
