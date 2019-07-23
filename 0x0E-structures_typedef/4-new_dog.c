cef9de73af032b30592b814636c9b957ed6dfa4c
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-23 06:30:28 +0000

    editing condtions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	new_dog->name = _strdup(name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = _strdup(owner);
 	return (new_dog);
+}
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in mem
+ * @str: char pointer
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *newstr;
+	char *tmp;
+	int len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len])
+	{
+		len++;
+	}
+	newstr = malloc(len + 1);
+	if (newstr == '\0')
+	{
+		return (NULL);
+	}
+	tmp = newstr;
+	while (*str)
+		*tmp++ = *str++;
+	*tmp = '\0';
+	return (newstr);
 }
