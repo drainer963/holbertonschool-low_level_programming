0c752882bb5ce8951d250472182d8a45988215bf
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-23 01:34:38 +0000

    print dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print struct elements.
+ * @d: struct pointer
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
