4caa68329b31ee0ab2e4a12056bf5941ef309028
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-24 21:03:28 +0000

    array iterator for function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - iterates an integer
+ * @array: the integer to print
+ * @size: size_t
+ * @action: void function pointer
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_H
 #include <stdio.h>
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
