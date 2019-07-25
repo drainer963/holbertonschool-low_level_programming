ddfb5f43c613d08643fc894e224933a86ee88d47
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-25 06:14:16 +0000

    first push

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,30 @@
+int op_add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+int op_sub(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+int op_mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
+int op_div(int a, int b)
+{
+	int sum = a / b;
+
+	return (sum);
+}
+int op_mod(int a, int b)
+{
+	int sum = a % b;
+
+	return (sum);
+}
