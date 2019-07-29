75250360d6cfe07e47e8d2ffc1eeea96ad8ae4e7
Trevor-Ofarrell <744@holbertonschool.com>
2019-07-29 01:48:34 +0000

    printf prototype

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,55 +1,92 @@
 #include "variadic_functions.h"
-
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-void car(va_list chr);
-void num(va_list dig);
-void dub(va_list flt);
-void p(va_list chrptr);
-
-void print_all(const char * const format, ...)
-{
-	int i = 0, j = 0;
-	va_list list;
-	op_t op[] = {
-	{"c" , car},
-	{"i" , num},
-	{"f" , dub},
-	{"s" , p},
-	};
-	va_start(list, format);
-	for (; format[i] != '\0'; i++)
-	{
-		for (; j < 4; j++)
-		{
-			if (format[i] == *op[j].c)
-			{
-				op[j].ptr(list);
-				printf(" ");
-			}
-		}
-	}
-	printf("\n");
-	va_end(list);
-}
 void car(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void num(va_list list)
 {
-	printf("%d", va_arg(list, int));
+        printf("%d", va_arg(list, int));
 }
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void dub(va_list list)
 {
-	printf("%f", va_arg(list, double));
+       printf("%f", va_arg(list, double));
 }
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void p(va_list list)
 {
-	if (list == NULL)
-		printf("(nil)");
-	printf("%s", va_arg(list, char *));
+        if ( list == NULL)
+                printf("(nil)");
+        printf("%s", va_arg(list, char *));
+}
+/**
+ * get_op_func - convert symbol into function
+ * @s: string which may correspoind to function
+ *
+ * Return: function associated with s
+ */
+void op_char_ptr(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	char *sep = "";
+	int i, j = 0;
+
+	va_list list;
+
+	op_t op[] = {
+		{"c", car},
+		{"i", num},
+		{"f", dub},
+		{"s", p},
+		{NULL, NULL}
+	};
+	va_start(list, format);
+	while (format && format[i])
+	{
+		while (op[j].ch)
+		{
+			if (*op[j].ch == format[i])
+			{
+				printf("%s", sep);
+				op[j].ptr(list);
+				sep = ", ";
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(list);
 }
