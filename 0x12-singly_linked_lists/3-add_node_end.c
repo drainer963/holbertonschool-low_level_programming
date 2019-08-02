9648ec0d39e6cf917b1fb5653a99adb28fbbedb5
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-02 02:46:57 +0000

    add node to end of linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,6 +18,11 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	newNode->next = NULL;
 	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+	{
+		free(newNode);
+		return(NULL);
+	}
 	newNode->len = _strlen(newNode->str);
 	if (!*head)
 	{
