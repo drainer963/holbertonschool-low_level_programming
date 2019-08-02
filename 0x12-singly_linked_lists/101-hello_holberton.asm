610680db66d0667abc4e81a38c2ed87062d64901
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-02 04:53:32 +0000

    asm \n == ,13,10,

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
-global main
-	section .text
+ section .data
+	fmt     db "%u  %s",10,0
+	msg1    db "Hello, Holberton",13,10
 
-main:
-	  mov rax, 1        	; write(
-	  mov rdi, 1        	;   STDOUT_FILENO,
-	  mov rsi, msg      	;   "Hello, world!\n",
-	  mov rdx, msglen   	;   sizeof("Hello, world!\n")
-	  syscall           	; );
+	    section .text
+	    extern printf
+	    global _start
 
-	  mov rax, 60       	; exit(
-	  mov rdi, 0        	;   EXIT_SUCCESS
-	  syscall           	; );
+_start:
+	    mov  edx, msg1
+	    mov  esi, 1
+	    mov  edi, fmt
+	    mov  eax, 0     	; no f.p. args
+	    call printf
 
-	section .data
-msg:		 db "Hello, Holberton", 13, 10,
-msglen:		 equ $ - msg
+	    mov  ebx, 0     	; return value
+	    mov  eax, 1
+	    int  0x80
