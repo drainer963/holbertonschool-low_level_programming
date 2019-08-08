d6c2c1327d8683c694a15e3b9c2f821ab573dff4
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-08 22:08:08 +0000

    find value of bit at given point

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,19 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i = 0, *bits = malloc(sizeof(int) * index);
-	unsigned int mask, maskN, bit;
-
-	if (!bits || !n)
+	if (!n)
 		return (-1);
-
-	while (i < index)
-	{
-		i++;
-		mask = 1 << i;
-		maskN = n & mask;
-		bit = maskN >> i;
-		*bits = bit;
-	}
-	return (*bits);
+	return (n >> index & 1);
 }
