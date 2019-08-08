b4f6d258a0ffddab1e7a224d8707ecb79edcd78c
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-08 22:49:00 +0000

    toggle bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * set_bit - toggle bits to 1
+ * @n: UL int.
+ * @index: UI
+ *
+ * Return: Always 0.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int new = *n;
+
+	new ^= 1UL << index;
+	*n = new;
+	return (n ? 1 : -1);
+}
