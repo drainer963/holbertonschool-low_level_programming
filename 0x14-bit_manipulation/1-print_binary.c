ce5f26fd46a66525a9645db2578acfc7d1264db2
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-08 21:19:07 +0000

    optimizing

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,15 +12,9 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 		_putchar('0');
-
 	while (i > 0)
 	{
-		if ((n & i) == 0)
-		{
-			_putchar('0');
-		}
-		else
-			_putchar('1');
+		_putchar(n & i ? '1' : '0');
 		i = i >> 1;
 	}
 }
