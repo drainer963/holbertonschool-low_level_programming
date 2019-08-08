d4da0a9afee2dcd96edb84509dc0ca0f5d99f77d
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-08 19:49:29 +0000

    print binary from decinal

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_binary - convert numbers into binary
+ * @n: unsigned long.
+ *
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+	int i = n;
+
+	while (i > 0)
+	{
+		if ((n & i) == 0)
+		{
+			_putchar('0');
+		}
+		else
+			_putchar('1');
+		i = i >> 1;
+	}
+		_putchar(0);
+}
