b8915f469fa58f7989a45df3b54907ab1efa9eb7
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-08 18:54:12 +0000

    first commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: const char pointer.
+ *
+ * Return: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+
+	if (*b == '\0')
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+		n *= 2;
+		if (*b++ == '1')
+		{
+			n++;
+		}
+	}
+	return (n);
+}
