bf68d4b8fc776a3e0313a8ba109c96feeb7ab7a7
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-09 00:44:52 +0000

    check endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - check for either little or big endian.
+ *
+ * Return: void
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	return (0);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int countSetBits(int n);
+
+int get_endianness(void);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
