85fcbb5e36fc87d32cbacae3ff89dd46874050d4
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-13 19:41:39 +0000

    open, read, write, close sys call

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * read_textfile - read text and write it to stdout using open, read, wri
+ * te, and close system calls.
+ * @filename: const char pointer
+ * @letters: size_t char count
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, ret;
+	size_t  bytes;
+	char *buf = malloc(sizeof(size_t) * letters);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDWR);
+	if (fd < 0)
+		return (0);
+	ret = read(fd, buf, letters);
+	buf[letters] = '\0';
+
+	bytes = write(1, buf, ret);
+	if (!bytes || bytes < letters)
+		return (0);
+	close(fd);
+
+	return (bytes);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
