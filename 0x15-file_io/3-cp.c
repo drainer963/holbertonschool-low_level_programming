4c3093a707b542777086282df2bba35eb5bf2e70
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-14 03:59:33 +0000

    copy files using sys calls

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+
+#define SIZE 1024
+#define INCORRECT "Usage: cp file_from file_to\n"
+#define READ "Error: Can't read from file %s\n"
+#define WRITE "Error: Can't write to %s\n"
+#define CLOSE "Error: Can't close fd %d\n"
+/**
+ * main - starting point of this program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd, fd2, ret = 0;
+	char buf[SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, INCORRECT), exit(97);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, READ, argv[1]), exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, WRITE, argv[2]), exit(99);
+	}
+	ret = read(fd, buf, BUFSIZ);
+	if (ret == -1)
+	{
+		dprintf(STDERR_FILENO, READ, argv[1]);
+		exit(98);
+	}
+	ret = write(fd2, buf, ret);
+	if (ret == -1)
+	{
+		dprintf(STDERR_FILENO, WRITE, argv[2]);
+		exit(99);
+	}
+	close(fd);
+	close(fd2);
+	if (fd)
+	{
+		dprintf(STDERR_FILENO, CLOSE, fd), exit(100);
+	}
+	if (fd2)
+	{
+		dprintf(STDERR_FILENO, CLOSE, fd), exit(100);
+	}
+	return (0);
+}
