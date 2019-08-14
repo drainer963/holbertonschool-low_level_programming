eb2bcfa0cce46674424b1d7d1802a3ac7ce1e212
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-14 00:06:07 +0000

    adding str len

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,16 +10,28 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t ret = 0;
+	size_t ret = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
-	ret = write(STDOUT_FILENO, text_content, ret);
-	if (!ret)
-		return (-1);
+	write(STDOUT_FILENO, text_content, ret);
 	close(fd);
 	return (1);
+}
+/**
+ * _strlen - return the length of a string
+ * @s: char pointer
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0, i = 0;
+
+	for (; s[i]; i++)
+		len++;
+	return (len);
 }
