6fbffb919356abef15a6bfced2cea35787c72990
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-14 00:55:22 +0000

    fixing conditions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,16 +14,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (len)
+	if (text_content != NULL)
 		len2 = write(fd, text_content, len);
 	close(fd);
 	if (len2 == -1)
-	{
 		return (-1);
-	}
 	return (1);
 }
 /**
