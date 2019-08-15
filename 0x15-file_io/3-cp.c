816d1e52d53ea841f9111a69efe07c5ca64327e8
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-15 02:00:45 +0000

    chaning conditions and return

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 #define SIZE 1024
 #define INCORRECT "Usage: cp file_from file_to\n"
 #define READ "Error: Can't read from file %s\n"
@@ -21,18 +22,18 @@ void closeFile(int fd)
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0
+ * Return: 1
  */
 int main(int argc, char *argv[])
 {
-	int fd, fd2, ret = 0, ret2 = 0;
+	int fd = 0, fd2 = 0, ret = 0;
 	char buf[SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, INCORRECT), exit(97);
 	}
-	fd = open(argv[1], O_RDWR);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, READ, argv[1]), exit(98);
@@ -45,14 +46,14 @@ int main(int argc, char *argv[])
 		closeFile(fd);
 		closeFile(fd2);
 	}
-	while (ret = read(fd, buf, SIZE))
+	while ((ret = read(fd, buf, SIZE)) > 0)
 	{
-		if (ret == -1)
-			dprintf(STDERR_FILENO, READ, argv[1]), exit(98);
 		if (write(fd2, buf, ret) != ret)
 			dprintf(STDERR_FILENO, WRITE, argv[2]), exit(99);
 	}
+	if (ret == -1)
+		dprintf(STDERR_FILENO, READ, argv[1]), exit(98);
 	closeFile(fd);
 	closeFile(fd2);
-	return (0);
+	return (1);
 }
