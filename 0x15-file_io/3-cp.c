9c6d6c6ebb24db891aaa96153c81784168c43a46
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-15 01:39:18 +0000

    program to read a file and write its contents to another using system calls

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,12 +1,23 @@
 #include "holberton.h"
-
 #define SIZE 1024
 #define INCORRECT "Usage: cp file_from file_to\n"
-#define READ "Error: Can't read from file %s\n"
+#define READ "Error: Can't read from %s\n"
 #define WRITE "Error: Can't write to %s\n"
 #define CLOSE "Error: Can't close fd %d\n"
 /**
- * main - starting point of this program
+ * closeFile - helper function to close file
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void closeFile(int fd)
+{
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, CLOSE, fd), exit(100);
+}
+/**
+ * main - program to copy file_from to file_to using open, read, write, and
+ * close system calls
  * @argc: argument count
  * @argv: argument vector
  *
@@ -14,43 +25,34 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd, fd2, ret = 0;
+	int fd, fd2, ret = 0, ret2 = 0;
 	char buf[SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, INCORRECT), exit(97);
 	}
-	fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDWR);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, READ, argv[1]), exit(98);
+		closeFile(fd);
 	}
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, WRITE, argv[2]), exit(99);
-
+		closeFile(fd);
+		closeFile(fd2);
 	}
 	while (ret = read(fd, buf, SIZE))
 	{
 		if (ret == -1)
-		{
 			dprintf(STDERR_FILENO, READ, argv[1]), exit(98);
-			close(fd);
-			close(fd2);
-		}
-		ret = write(fd2, buf, ret);
-		if (ret == -1)
-		{
+		if (write(fd2, buf, ret) != ret)
 			dprintf(STDERR_FILENO, WRITE, argv[2]), exit(99);
-		}
-		close(fd);
-		close(fd2);
 	}
-	if (fd)
-		dprintf(STDERR_FILENO, CLOSE, fd), exit(100);
-	if (fd2)
-		dprintf(STDERR_FILENO, CLOSE, fd), exit(100);
+	closeFile(fd);
+	closeFile(fd2);
 	return (0);
 }
