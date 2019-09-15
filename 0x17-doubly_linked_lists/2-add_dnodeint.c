417d5a04103d0f678547d1a0d5071d19599a2b55
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-15 06:14:24 +0000

    add node to head of linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node to start of doubly linked list.
+ * @head: head node
+ * @n: data
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (head == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (!newnode->n)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->next = (*head);
+	newnode->prev = NULL;
+	(*head) = newnode;
+	return (newnode);
+}
