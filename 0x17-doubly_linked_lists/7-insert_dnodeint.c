3198f1a3aad9071e88eb04905ecc55100414376c
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-15 22:00:32 +0000

    insert node at given indeex

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert node at given index.
+ * @h: head node
+ * @idx: index
+ * @n: data
+ *
+ * Return: newnode
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *current = *h, *newnode = malloc(sizeof(dlistint_t));
+
+	newnode->n = n;
+	while (current != NULL)
+	{
+		if (i == idx - 1)
+		{
+			newnode->next = current->next;
+			current->next = newnode;
+			return (newnode);
+		}
+		i++;
+		current = current->next;
+	}
+	return (NULL);
+}
