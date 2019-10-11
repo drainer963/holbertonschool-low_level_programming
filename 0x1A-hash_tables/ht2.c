6394e136a4f7a034484a67e810536b94ac4a8e72
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-11 04:31:52 +0000

    working hash table

--- 0x1A-hash_tables/ht2.c
+++ 0x1A-hash_tables/ht2.c
@@ -14,8 +14,89 @@ unsigned int hash(const char *key)
 	value = value % TABLE_SIZE;
 	return (value);
 }
+
+hash_node_t *ht_pair(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t) * 1);
+
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
+
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+
+	node->next = NULL;
+
+	return (node);
+}
+
+void ht_set(hash_table_t *hashtable, const char *key, const char *value)
+{
+	unsigned int slot = hash(key);
+	hash_node_t *node = hashtable->array[slot];
+
+	if (node == NULL)
+	{
+		hashtable->array[slot] = ht_pair(key, value);
+		return;
+	}
+
+	hash_node_t *prev;
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->key = malloc(strlen(value) + 1);
+			strcpy(node->value, value);
+			return;
+		}
+
+		prev = node;
+		node = prev->next;
+	}
+	prev->next = ht_pair(key, value);
+}
+
+char *ht_get(hash_table_t *hashtable, const char *key)
+{
+	unsigned int slot = hash(key);
+	hash_node_t *node = hashtable->array[slot];
+
+	if (node == NULL)
+	{
+		return (NULL);\
+	}
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+}
+
+hash_table_t *hash_table_create(unsigned long int size);
+{
+	hash_table_t *hashtable = malloc(sizeof(hash_table_t) * 1);
+	int i = 0;
+
+	hashtable->array = malloc(sizeof(hash_node_t*) * TABLE_SIZE);
+
+	for (i = 0; i < TABLE_SIZE; ++i)
+	{
+		hashtable->array[i] = NULL;
+	}
+	return (hashtable);
+}
+
 int main()
 {
-	printf("%d\n", hash("fuck you"));
-	return (0);
+	hash_table_t *ht = hash_table_create();
+
+	printf("%p\n", ht);
+
+	return;
 }
