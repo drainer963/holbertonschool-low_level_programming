1a8a0fc4ed0d9036d36c75eeaebc2e4013359fca
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-11 22:41:29 +0000

    adding functions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,9 @@ int hash_table_set(hash_table_t *ht, char *key, char *value)
 	hash_node_t *node = ht->array[slot];
 	hash_node_t *prev;
 
+	if (!key)
+		return (0);
+
 	if (node == NULL)
 	{
 		ht->array[slot] = ht_pair(key, value);
@@ -33,4 +36,42 @@ int hash_table_set(hash_table_t *ht, char *key, char *value)
 	}
 	prev->next = ht_pair(key, value);
 	return (0);
+}
+
+/**
+ * ht_pair - initializes keys value pairs.
+ * @key: const char
+ * @value: const char
+ * Return: pointer to node
+ */
+hash_node_t *ht_pair(char *key, char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t) * 1);
+
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
+
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+
+	node->next = NULL;
+
+	return (node);
+}
+
+/**
+ * hash - hashes values for keys.
+ * @key: const char pointer to keys in table
+ *
+ * Return: pointer to value
+ */
+unsigned long hash(char *str)
+{
+	unsigned int hash = 0;
+	int c;
+
+	while ((c = *str++))
+		hash += c;
+
+	return hash;
 }
