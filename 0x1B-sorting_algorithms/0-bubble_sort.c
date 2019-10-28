fce7d78e09e281ba58628b01cc279444d46a2db8
trevor-ofarrell <744@holbertonschool.com>
2019-10-28 23:03:05 +0000

    betty

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include "sort.h"
-
+/**
+ * swap - swap pointer values
+ * @xp: x pointer
+ * @yp: y pointer
+ *
+ * Return: Always 0
+ */
 void swap(int *xp, int *yp)
 {
-  int temp = *xp;
+	int temp = *xp;
 
-  *xp = *yp;
-  *yp = temp;
+	*xp = *yp;
+	*yp = temp;
 }
-
+/**
+ * bubble_sort - sort using bubble sort algorithm
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
 void bubble_sort(int *array, size_t size)
 {
-  unsigned int i, j;
+	unsigned int i, j;
 
-  for (i = 0; i < size - 1; i++)
-
-    for (j = 0; j < size - i - 1; j++)
-
-      if (array[j] > array[j + 1])
-	{
-	  swap(&array[j], &array[j + 1]);
-	  print_array(array, size);
-	}
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
 }
