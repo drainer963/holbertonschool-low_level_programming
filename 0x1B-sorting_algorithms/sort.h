ef2dcf0beedf860cf7b46bc4a947d26bbeba6887
trevor-ofarrell <744@holbertonschool.com>
2019-10-29 03:41:50 +0000

    rename file

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,7 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
