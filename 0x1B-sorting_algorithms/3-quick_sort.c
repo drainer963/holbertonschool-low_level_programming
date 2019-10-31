b870ffdb44c67370d46e191d9d68d4254b791298
trevor-ofarrell <744@holbertonschool.com>
2019-10-31 04:45:31 +0000

    quick sort

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+/**
+ * quick_sort - quick sort algorithm
+ *
+ * @array: int array
+ * @size: array size
+ * Return: void
+ */
+void quick_sort(int array[], size_t size)
+{
+  int first = 0;
+
+  quick_sort_2(array, first, size - 1, size);
+}
+/**
+ * quick_sort_2 - Entry point
+ *
+ * @array: int array
+ * @low: first element
+ * @high: last element
+ * @size: array size
+ * Return: void
+ */
+void quick_sort_2(int *array, int low, int high, size_t size)
+{
+  int pi;
+
+  if (low < high)
+    {
+      pi = part(array, low, high, size);
+      quick_sort_2(array, low, pi - 1, size);
+      quick_sort_2(array, pi + 1, high, size);
+    }
+}
+/**
+ * part - partition array
+ *
+ * @array: int array
+ * @first: first element
+ * @last: last element
+ * @size: array size
+ * Return: void
+ */
+int part(int *array, int first, int last, size_t size)
+{
+  int pivot = array[last], j = 0;
+  int i = first - 1;
+
+  for (j = first; j < last; j++)
+    {
+      if (array[j] < pivot)
+	{
+	  ++i;
+	  swap(&array[i], &array[j]);
+	  print_array(array, size);
+	}
+    }
+  swap(&array[i + 1], &array[last]);
+  print_array(array, size);
+  return (i + 1);
+}
+/**
+ * main - Entry point
+ * @a: int pointer
+ * @b: int pointer
+ *
+ * Return: void
+ */
+void swap(int* a, int* b)
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -18,6 +18,9 @@ typedef struct listint_s
 
 void swap(int *xp, int *yp);
 void selection_sort(int *array, size_t size);
+int part(int array[], int first, int last, size_t size);
+void quick_sort_2(int *array, int first, int last, size_t size);
+void quick_sort(int *array, size_t size);
 void insertion_sort_list_rec(listint_t **list, int len);
 int countnodes(listint_t *head);
 void sortedInsert(listint_t** head_ref, listint_t* new_node);
