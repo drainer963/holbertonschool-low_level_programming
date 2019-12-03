95e754585642b50dc61e9a29ce5aafaa357a4400
Trevor <trevor-ofarrell>
2019-12-03 15:35:13 -0800

    initial commit

--- 0x1D-binary-trees/binary_trees.h
+++ 0x1D-binary-trees/binary_trees.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -24,6 +25,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_root(const binary_tree_t *node);
 int binary_tree_is_leaf(const binary_tree_t *node);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
