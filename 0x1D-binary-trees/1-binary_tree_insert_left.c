050f71e5d14853292efdca79d99cbac9521b9ae4
Trevor <trevor-ofarrell>
2019-12-03 13:18:27 -0800

    initial commit

---
+++ 0x1D-binary-trees/1-binary_tree_insert_left.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+  binary_tree_t *new = (binary_tree_t*) malloc(sizeof(binary_tree_t));
+
+  
+}
