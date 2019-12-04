156a4fae19777e1de96731e305294ebd2a0aeaf1
Trevor <trevor-ofarrell>
2019-12-04 13:22:57 -0800

    initial commit

--- 0x1D-binary-trees/1-binary_tree_insert_left.c
+++ 0x1D-binary-trees/1-binary_tree_insert_left.c
@@ -2,10 +2,23 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new = (binary_tree_t*) malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-  new->parent = parent;
-  new->n = value;
-  new->left;
-  new->right;
+	if (!parent)
+		return (NULL);
+
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = parent->left;
+	if (new->left)
+		new->left->parent = new;
+	new->right = NULL;
+
+	parent->left = new;
+
+	return (new);
 }
