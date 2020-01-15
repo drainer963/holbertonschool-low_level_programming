c4b9042ceada62a1036219594aa9ad3dcbd3a178
trevor-ofarrell <744@holbertonschool.com>
2020-01-15 13:44:23 -0800

    linting

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,16 +9,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-  size_t i = 0;
-  
-  for (i = 0; i < size; i++)
-    {
-      if (array[i] == value)
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
 	{
-	  printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-	  return(i);
+		if (array[i] == value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 	}
-      printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-    }
-  return (-1);
+	return (-1);
 }
